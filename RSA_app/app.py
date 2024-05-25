d2092887d0226368296a8901934e5c2fb566cfa3
TheNushu <131345754+TheNushu@users.noreply.github.com>
2024-05-26 00:02:30 +0300

    Cleaned code app.py

--- RSA_app/app.py
+++ RSA_app/app.py
@@ -13,8 +13,6 @@ def function2():
     result = input_text1 + " " + input_text2  
     output_text.set(result)
 
-
-
 # Create the main window
 root = tk.Tk()
 root.title("Save my secret!!")
@@ -46,7 +44,6 @@ button3 = tk.Button(root,
                     text="Generate pair of keys", 
                     command=generate_keys)
 
-
 # Layout widgets
 label1.grid(row=0, column=0, padx=10, pady=10)
 entry1.grid(row=0, column=1, padx=10, pady=10)
@@ -72,59 +69,3 @@ output_label.grid(row=3, column=0,
 
 # Start the GUI event loop
 root.mainloop()
-
-
-
-'''import tkinter as tk
-from tkinter import messagebox
-import rsa
-
-class RSAApp:
-    def __init__(self, master):
-        self.master = master
-        self.master.title("RSA Encryption Tool")
-        self.public_key = None
-        self.private_key = None
-
-        self.txt_message = tk.Text(master, height=5, width=50)
-        self.txt_message.pack(pady=5)
-
-        self.txt_encrypted = tk.Text(master, height=5, width=50)
-        self.txt_encrypted.pack(pady=5)
-
-        self.txt_decrypted = tk.Text(master, height=5, width=50)
-        self.txt_decrypted.pack(pady=5)
-
-        tk.Button(master, text="Generate Keys", command=self.generate_keys_command).pack(pady=5)
-        tk.Button(master, text="Encrypt", command=self.encrypt_message).pack(pady=5)
-        tk.Button(master, text="Decrypt", command=self.decrypt_message).pack(pady=5)
-
-    def generate_keys_command(self):
-        self.public_key, self.private_key = rsa.newkeys(512)
-        print(self.private_key)
-        print(self.public_key)
-        messagebox.showinfo("Keys", "Keys generated successfully!")
-
-    def encrypt_message(self):
-        encoded = message.encode("utf8")
-        message = encoded.get("1.0", "end-1c")
-        if not self.public_key:
-            messagebox.showerror("Error", "Generate keys first!")
-            return
-        encrypted_msg = rsa.encrypt(message, self.public_key)
-        self.txt_encrypted.delete("1.0", tk.END)
-        self.txt_encrypted.insert(tk.END, encrypted_msg)
-
-    def decrypt_message(self):
-        encrypted_message = self.txt_encrypted.get("1.0", "end-1c")
-        if not self.private_key:
-            messagebox.showerror("Error", "Generate keys first!")
-            return
-        decrypted_msg = rsa.decrypt(encrypted_message, self.private_key)
-        self.txt_decrypted.delete("1.0", tk.END)
-        self.txt_decrypted.insert(tk.END, decrypted_msg)
-
-app = tk.Tk()
-rsa_app = RSAApp(app)
-app.mainloop()
-'''
