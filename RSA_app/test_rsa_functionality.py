e67b8dae3686bd72379e3826361d765726d9b218
Mlesnita Daniel I <mlesdani@cubbli22-gold.local>
2024-06-15 23:14:54 +0300

    Improve unittests

--- RSA_app/test_rsa_functionality.py
+++ RSA_app/test_rsa_functionality.py
@@ -16,8 +16,8 @@ class TestRSAGeneration(unittest.TestCase):
 
     def test_n_bit_random(self):
         """Test random number generation for various bit lengths."""
-        BITS = [2, 4, 256, 512]
-        for n in BITS:
+        bits = [4, 256, 512, 1024]
+        for n in bits:
             rand_num = generate_n_bit_random(n)
             self.assertTrue(
                 2**(n-1) <= rand_num < 2**n,
@@ -26,32 +26,51 @@ class TestRSAGeneration(unittest.TestCase):
 
     def test_low_level_prime_generation(self):
         """Test the generation of low-level prime numbers."""
-        prime = get_low_level_prime(1024)
-        self.assertFalse(
-            any(prime % p == 0 for p in FIRST_PRIMES_LIST if p**2 <= prime),
-            "Generated number is divisible by one of the first primes list"
-        )
+        bits = [4, 256, 512, 1024]
+
+        for n in bits:
+            prime = get_low_level_prime(n)
+            self.assertFalse(
+                any(prime % p == 0 for p in FIRST_PRIMES_LIST if p**2 <= prime),
+                "Generated number is divisible by one of the first primeslist"
+            )
 
     def test_miller_rabin(self):
-        self.assertTrue(is_miller_rabin_passed(101), "101 should be prime")
-        self.assertFalse(is_miller_rabin_passed(100), "100 should not be prime")
+
+        primes= [101,
+                  2147483647,
+                  100000004749,
+                  24815323469403931728221172233738523533528335161133543380459461440894543366372904768334987264000000000000000000479,
+                  ]
+
+        non_primes= [87,
+                     98764321234,
+                     12358848653920354053021237345,
+                     1235884865392035405302123734711111111111111111111111111111111111111111257]
+
+        for prime in primes:
+            self.assertTrue(is_miller_rabin_passed(prime),
+                            f"{prime} should be prime")
+
+        for non_prime in non_primes:
+            self.assertFalse(is_miller_rabin_passed(non_prime),
+                             f"{non_prime} should not be prime")
 
     def test_generate_prime(self):
-        prime = generate_prime(20)
-        self.assertTrue(is_miller_rabin_passed(prime), "Generated number should be prime")
+        bits = [32, 128, 512]
 
-    def test_generate_keys(self):
-        public_key, private_key = generate_keys()
-        e, n = public_key
-        d, _ = private_key
-        phi_n = n - (e * d % n)
-        self.assertEqual(
-            e * d % phi_n, 1,
-            "Public and private keys are not inverses modulo φ(n)"
-        )
+        for n in bits:
+            prime = generate_prime(n)
+            self.assertTrue(is_miller_rabin_passed(prime),
+                            f"{prime} should be prime")
+            self.assertEqual(prime.bit_length(), n,
+                             f"{prime} should have {n} bits")
+            another_prime = generate_prime(n)
+            self.assertFalse(is_miller_rabin_passed(prime * another_prime),
+                             f"{prime*another_prime} should not be considered prime")
 
     def test_encryption_decryption(self):
-        message = "Test message"
+        message = "Test message 12!.'s@[]"
         public_key, private_key = generate_keys()
         encrypted = encrypt_message(message, public_key)
         decrypted = decrypt_message(encrypted, private_key)
@@ -63,9 +82,23 @@ class TestRSAGeneration(unittest.TestCase):
     def test_input_validations(self):
         with self.assertRaises(ValueError):
             generate_n_bit_random(-1)  # Test negative bit length
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg="encrypt should fail on inproper input"):
             encrypt_message(12345, (65537, 99991))  # Non-string message input
+            encrypt_message("Meow", "Woof") # Non-tuple public key
+            encrypt_message("Meow", ("Woof", "Quack")) # Non-int key
 
-
+            decrypt_message("Meow", "Woof") # Non-tuple public key
+            decrypt_message("Meow", ("Woof", "Quack")) # Non-int key
+    """
+    def test_generate_keys(self):
+        public_key, private_key = generate_keys()
+        e, n = public_key
+        d, _ = private_key
+        phi_n = n - (e * d % n)
+        self.assertEqual(
+            e * d % phi_n, 1,
+            "Public and private keys are not inverses modulo φ(n)"
+        )
+    """
 if __name__ == '__main__':
     unittest.main()
